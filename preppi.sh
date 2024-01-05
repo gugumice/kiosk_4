cdc1a528c3aeeec4c37847f794cc7bcafef9e64f
Gunars Frickauss <gunarsf@gmail.com>
2024-01-05 16:23:39 +0200

    Update preppi.sh

--- preppi.sh
+++ preppi.sh
@@ -12,7 +12,6 @@ systemctl enable firstboot.service
 timedatectl set-timezone Europe/Riga
 sed -i '/^# Additional overlays.*/a dtoverlay=pi3-disable-wifi\ndtoverlay=pi3-disable-bt' /boot/config.txt
 sed -i '/^\[all\].*/a gpu_mem=16' /boot/config.txt
-sed -i '/^\[global\]$/a break-system-packages = true' /etc/pip.conf
 
 sleep 3
 apt-get --yes install libcups2-dev cups cups-bsd
@@ -27,6 +26,8 @@ addgroup watchdog
 usermod -a -G watchdog pi
 service cups restart
 apt-get --yes install python3-pip
+sed -i '/^\[global\]$/a break-system-packages = true' /etc/pip.conf
+
 #apt-get --yes --allow-downgrades --allow-remove-essential --allow-change-held-packages install python3-pip
 echo 'KERNEL=="watchdog", MODE="0660", GROUP="watchdog"' > /etc/udev/rules.d/60-watchdog.rules
 sed -i '/^#NTP=.*/a FallbackNTP=laiks.egl.local' /etc/systemd/timesyncd.conf
