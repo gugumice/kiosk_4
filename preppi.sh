96682330a32aa45ae0adc9dc18487eec0e6c9480
Gunars Frickauss <gunarsf@gmail.com>
2024-05-07 13:53:29 +0300

    Update preppi.sh

--- preppi.sh
+++ preppi.sh
@@ -12,8 +12,6 @@ systemctl enable firstboot.service
 timedatectl set-timezone Europe/Riga
 sed -i '/^# Additional overlays.*/a dtoverlay=pi3-disable-wifi\ndtoverlay=pi3-disable-bt' /boot/config.txt
 sed -i '/^\[all\].*/a gpu_mem=16' /boot/config.txt
-
-sleep 3
 apt-get --yes install libcups2-dev cups cups-bsd
 cupsctl --remote-admin --remote-any
 usermod -a -G lpadmin pi
@@ -31,9 +29,7 @@ sed -i '/^\[global\]$/a break-system-packages = true' /etc/pip.conf
 #apt-get --yes --allow-downgrades --allow-remove-essential --allow-change-held-packages install python3-pip
 echo 'KERNEL=="watchdog", MODE="0660", GROUP="watchdog"' > /etc/udev/rules.d/60-watchdog.rules
 sed -i '/^#NTP=.*/a FallbackNTP=laiks.egl.local' /etc/systemd/timesyncd.conf
-#chattr -i /etc/hosts
 echo '10.100.20.104   laiks.egl.local' >> /etc/hosts
-#chattr +i /etc/hosts
 pip3 --no-input install requests pyserial configparser
 pip3 --no-input install pycups
 #/usr/sbin/shutdown -r now
